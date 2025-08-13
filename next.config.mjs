2b16206354f3ea27766880da39f8f456b074e7a2
rpradeepraj <rpredeepraj@gmail.com>
2025-08-13 09:43:31 +0530

    changed the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
+  basePath: '/PRAXOR', 
+  assetPrefix: '/PRAXOR/', 
   images: {
     unoptimized: true // required for static export if using <Image>
   },
