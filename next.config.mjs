32dd2dd08ae558b725c1d4f8ade1bfff96e4f536
rpradeepraj <rpredeepraj@gmail.com>
2025-08-13 09:17:49 +0530

    build changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
+  images: {
+    unoptimized: true // required for static export if using <Image>
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
